d9db8c3afce958dad4d5fe5071f70ebbc3aed828
zhshxlzt <2284174996@qq.com>
2019-08-24 17:27:05 +0000

    修改

--- ServerTest/Program.cs
+++ ServerTest/Program.cs
@@ -13,19 +13,19 @@ namespace ServerTest
     {
         static void Main( string[] args )
         {
-            //TcpListener listener = new TcpListener(IPAddress.Parse("192.168.1.112"), 8321);
-            //listener.Start();
-            //TcpClient x = listener.AcceptTcpClient();
-            //Console.Write("客户端连接状态：" + x.Client.Connected.ToString());
-            //byte[] byteInfo = new byte[256];
-            //x.Client.Receive(byteInfo);
-            //Console.Write("收到的数据长度: " + byteInfo.Length.ToString());
-            //var msg = Encoding.UTF8.GetString(byteInfo);
-            //Console.Write("信息：" + msg);
-            //Console.ReadLine();
+            TcpListener listener = new TcpListener(IPAddress.Parse("81.90.189.23"), 8321);
+            listener.Start();
+            TcpClient x = listener.AcceptTcpClient();
+            Console.Write("客户端连接状态：" + x.Client.Connected.ToString());
+            byte[] byteInfo = new byte[256];
+            x.Client.Receive(byteInfo);
+            Console.Write("收到的数据长度: " + byteInfo.Length.ToString());
+            var msg = Encoding.UTF8.GetString(byteInfo);
+            Console.Write("信息：" + msg);
+            Console.ReadLine();
             Console.WriteLine("主线程ID: " + Thread.GetDomainID());
-            AddAsync(1, 2);
-            Thread.Sleep(1000);
+            //AddAsync(1, 2);
+            //Thread.Sleep(1000);
             Console.ReadLine();
         }
 
